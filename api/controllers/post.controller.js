68f83a1ee5868677a69dba3ebf3dad056b6a6bdd
Khushi <khushikeserwani3830@gmail.com>
2024-04-10 21:46:08 +0530

    getposts api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,5 +1,6 @@
 import postModel from "../models/postModel.js";
-import { errorHandler } from "../utils/error.js"
+import { errorHandler } from "../utils/error.js";
+
 
 
 export const createPostController=async(req,res,next)=>{
@@ -30,4 +31,57 @@ try {
 }
 
 
+}
+
+//getting all posts
+ export const getPosts=async(req,res,next)=>{
+
+    try {
+
+        const startIndex=parseInt(req.query.startIndex) || 0;
+        const limit=parseInt(req.query.limit) || 9;//suppose
+        const sortDirection=req.query.order=== 'asc' ? 1:-1;
+
+        const posts=await postModel.find({
+            ...(req.query.userId && {userId:req.query.userId}),
+            ...(req.query.category && {category:req.query.category}),
+            ...(req.query.slug && {slug:req.query.slug}),
+            ...(req.query.postId && {_id:req.query.postId}),
+            //for serch terms
+            ...(req.query.serchTerms && {
+                $or:[
+                    { title :{$regex :req.query.serchTerms, $options:'i'}},
+                    { content :{$regex :req.query.serchTerms, $options:'i'}},
+                ],
+            }),
+         }).sort({updatedAt:sortDirection}).skip(startIndex).limit(limit); //find ended
+
+
+         //for total number of posts in our database
+         const totalPosts=await postModel.countDocuments();
+
+
+       //for getting number of posts created in last month or a month ago
+         const now=new Date();
+        var oneMonthAgo=new Date(
+            now.getFullYear(),
+            now.getMonth()-1,
+            now.getDate()
+        );
+
+     var lastMonthPosts=await postModel.countDocuments({
+        createdAt:{$gte: oneMonthAgo},
+     });
+
+
+     res.status(200).json({
+        posts,
+        totalPosts,
+        lastMonthPosts
+     });
+
+        
+    } catch (error) {
+        next(error);
+    }
 }
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyuser.js';
-import { createPostController } from '../controllers/post.controller.js';
+import { createPostController, getPosts } from '../controllers/post.controller.js';
 
 
 const router=express.Router();
 
 router.post('/create',verifyToken,createPostController);
+router.get('/getposts',getPosts);
 
 
 
