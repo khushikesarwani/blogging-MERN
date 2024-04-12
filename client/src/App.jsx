1f254bf6aa46574ce2765342c557f53cfacac780
Khushi <khushikeserwani3830@gmail.com>
2024-04-12 15:41:11 +0530

    complete post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from './components/PrivateRoute.jsx'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute.jsx';
 import CreatePost from './pages/CreatePost.jsx';
 import UpdatePost from './pages/UpdatePost.jsx';
+import PostPage from './pages/PostPage.jsx';
 
 
 
@@ -35,6 +36,8 @@ const App = () => {
         <Route path='/create-post' element={<CreatePost />} />
         <Route path='/update-post/:postId' element={<UpdatePost />} />
         </Route>
+
+      <Route path='/post/:postSlug' element={<PostPage />} />
 
       </Routes>
       <Footer />
